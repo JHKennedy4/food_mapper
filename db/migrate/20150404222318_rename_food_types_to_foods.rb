1121f4a8e6518219fbfd8c8d7f1e549e09adcdd7
John Kennedy <jhkennedy4@gmail.com>
2015-04-05 16:00:47 -0700

    so. much. data. entry.

--- db/migrate/20150404222318_rename_food_types_to_foods.rb
+++ db/migrate/20150404222318_rename_food_types_to_foods.rb
@@ -1,9 +1,5 @@
 class RenameFoodTypesToFoods < ActiveRecord::Migration
-  def self.up
+  def change
     rename_table :food_types, :foods
-  end
-
-  def self.down
-    rename_table :foods, :food_types
   end
 end
